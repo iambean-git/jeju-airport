3e04c22c7d8657d65896fd325b63e470354cdb98
iambean-git <peb1021@naver.com>
2025-03-24 14:52:34 +0900

    매우 혼잡 추가가

--- frontend/app/ui/boarding/boardingtimeDetail.tsx
+++ frontend/app/ui/boarding/boardingtimeDetail.tsx
@@ -9,25 +9,6 @@ const steps = [
     { id: 6, title: '출발', icon: '✈️' },
 ];
 
-const sectionCongestion = [
-    { id: 1, status: '혼잡' },
-    { id: 2, status: '여유' },
-    { id: 3, status: '보통' },
-];
-
-const sectionTime = [
-    { time: 16 },
-    { time: 2 },
-    { time: 36 },
-    { time: 24 }
-];
-
-const congestColor = {
-    "혼잡": "bg-red-500",
-    "여유": "bg-yellow-500",
-    "보통": "bg-green-500",
-}
-
 export default function BoardingTimeDetail(
     {time, congestion} : {
         time : BoardingTime,
@@ -35,7 +16,6 @@ export default function BoardingTimeDetail(
     }) {
     const nodeStyle = clsx(
         'h-20 w-[160px] md:w-1/6 relative border-l-4 md:border-t-4 md:border-l-0'
-        // 'h-20 w-2/3 relative border-l-4 border-gray-300'
     )
     
     return (
@@ -49,7 +29,8 @@ export default function BoardingTimeDetail(
                         <div key={`time${idx}`}
                             className='h-20  md:h-fit md:w-1/6 flex items-center justify-center translate-y-4.5'>
                             <span className={clsx('bg-gray-200 px-3 py-1 rounded-xl text-gray-700 text-xs', {
-                                "text-red-600 bg-red-100": idx < 3 && congestion[idx].status == "혼잡",
+                                "text-red-600 bg-red-100": idx < 3 && congestion[idx].status == "매우혼잡",
+                                "text-orange-600 bg-orange-100": idx < 3 && congestion[idx].status == "혼잡",
                                 "text-green-600 bg-green-100": idx < 3 && congestion[idx].status == "여유",
                                 "text-yellow-600 bg-yellow-100": idx < 3 && congestion[idx].status == "보통",
                             })}> {time.time}분 </span>
@@ -62,7 +43,8 @@ export default function BoardingTimeDetail(
                     <div className='h-20 md:w-1/6 '></div>
                     {steps.map((s) => (
                         <div className={clsx(nodeStyle, {
-                            "border-red-500": congestion[s.id - 2]?.status == "혼잡",
+                            "border-red-500": congestion[s.id - 2]?.status == "매우혼잡",
+                            "border-orange-500": congestion[s.id - 2]?.status == "혼잡",
                             "border-green-500": congestion[s.id - 2]?.status == "여유",
                             "border-yellow-500": congestion[s.id - 2]?.status == "보통",
                             "border-gray-300": s.id == 5 || s.id == 1,
@@ -87,8 +69,12 @@ export default function BoardingTimeDetail(
 
             {/* 혼잡도 범례 */}
             <div className="-mt-10 mb-5 flex items-center md:mt-16 space-x-4 justify-center z-10 ">
-                <div className="flex items-center">
+            <div className="flex items-center">
                     <div className="h-3 w-3 rounded-full bg-red-500 mr-1"></div>
+                    <span className="text-xs">매우혼잡</span>
+                </div>
+                <div className="flex items-center">
+                    <div className="h-3 w-3 rounded-full bg-orange-500 mr-1"></div>
                     <span className="text-xs">혼잡</span>
                 </div>
                 <div className="flex items-center">
